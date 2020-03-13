103c3fb3dd1a410565980b496c234f3e5f2c8a97
ssrokyz <ssrokyz@gmail.com>
2020-03-13 18:05:55 +0900

    reorder options for calculator and add big_sys option

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -14,6 +14,7 @@ class ss_calc(Calculator, object):
         self,
         model,
         var_ckpt,
+        big_sys=True,
         label='ss_calc',
         atoms=None,
         log_f='log.txt',
@@ -23,12 +24,17 @@ class ss_calc(Calculator, object):
             - (str)    Path to the saved model in pickle format.
             - (object) Object of one of these classes: (NN_force, ). Must have same training parameters used to make the var_ckpt file.
         var_ckpt (str) - Tensorflow variables checkpoint file.
+        big_sys (bool) - To save system memory, set 'True'.
+                         But for small systems, 'False' might give the wrong results.
+                         Recomandation) No a duplication of index in Neigh_ind for a atom. (Typically, more than 100 atoms) --> True
+                                        There are duplications.                            (Typically, less than 100 atoms) --> False
         """
 
         # Initialize
         Calculator.__init__(self, label=label, atoms=atoms)
 
         ## Global variables
+        self.big_sys = big_sys
         # Logger
         from ss_util import Logger
 
@@ -43,8 +49,7 @@ class ss_calc(Calculator, object):
         self.model.dscrptr.log = model.log
 
         # Build an NN
-        self.X, self.X_deriv, self.Neigh_ind, self.OL, F_hat, E_hat, self.DR = model.build_nn()
-        # self.E = self.get_energy_tensor()
+        self.X, self.X_deriv, self.Neigh_ind, self.OL, F_hat, E_hat, self.TAS_inv, self.DR = model.build_nn()
         self.F = self.get_force_tensor()
 
         # Load the saved variables
@@ -64,7 +69,7 @@ class ss_calc(Calculator, object):
         """
         # Reorder Neigh_ind
         #--> shape of (len_atoms, num_cutoff)
-        Neigh_ind = tf.concat(self.Neigh_ind, axis=0)
+        Neigh_ind = tf.gather(tf.concat(self.Neigh_ind, axis=0), self.TAS_inv)
 
         # Calc F_ij
         F_ij = []
@@ -74,44 +79,80 @@ class ss_calc(Calculator, object):
                 tf.reshape(tf.gradients(self.OL[spec], self.X[spec])[0], [-1, self.model.dscrptr.len_dscrptr, 1]),
                 ), [-1, self.model.dscrptr.num_cutoff, 3]))
         #--> shape of (len_atoms, num_cutoff, 3)
-        F_ij = tf.concat(F_ij, axis=0)
+        F_ij = tf.gather(tf.concat(F_ij, axis=0), self.TAS_inv)
 
         # First (self) term of forces.
-        #--> shape of (num_batch, len_atoms, 3)
+        #--> shape of (len_atoms, 3)
         f_self = -tf.reduce_sum(F_ij, axis=1)
 
         # Second (cross) term of forces.
         len_atoms = tf.shape(F_ij)[0]
-        a_bool = tf.equal(
-                    #--> shape of (len_atoms, len_atoms, num_cutoff)
-                    tf.tile(
-                        tf.expand_dims(Neigh_ind, axis=0),
-                        [len_atoms,1,1],
-                        ),
-                    tf.reshape(tf.range(len_atoms), [len_atoms,1,1]),
-                    )
 
-                # --> shape of (len_atoms, 3)
-        f_cross = tf.reduce_sum(
-            #--> shape of (len_atoms, len_atoms, 3)
-            tf.reduce_sum(
-                tf.ragged.boolean_mask(
-                    #--> shape of (len_atoms, len_atoms, num_cutoff, 3)
-                    tf.tile(
-                        tf.expand_dims(
-                            F_ij,
-                            axis=0,
-                            ),
-                        [len_atoms,1,1,1],
+        # Memory saving process.
+        if self.big_sys:
+            a_bool = tf.equal(
+                tf.reshape(
+                    tf.gather(
+                        Neigh_ind,
+                        tf.reshape(Neigh_ind, [-1]),
                         ),
-                    #--> shape of (len_atoms, len_atoms, num_cutoff)
-                    a_bool,
-                    name='neigh_mask',
+                    [len_atoms, self.model.dscrptr.num_cutoff, self.model.dscrptr.num_cutoff],
                     ),
-                axis=2,
-                ),
-            axis=1,
-            )
+                tf.reshape(tf.range(len_atoms), [len_atoms,1,1]),
+                )
+            #         --> shape of (len_atoms, 3)
+            f_cross = tf.reduce_sum(
+                #--> shape of (len_atoms, num_cutoff, 3)
+                tf.reduce_sum(
+                    tf.ragged.boolean_mask(
+                        tf.reshape(
+                            tf.gather(
+                                F_ij,
+                                tf.reshape(Neigh_ind, [-1]),
+                                ),
+                            [len_atoms, self.model.dscrptr.num_cutoff, self.model.dscrptr.num_cutoff, 3],
+                            ),
+                        #--> shape of (len_atoms, num_cutoff, num_cutoff)
+                        a_bool,
+                        name='neigh_mask',
+                        ),
+                    axis=2,
+                    ),
+                axis=1,
+                )
+
+        # Accurate for small systems. (Typically, less than 100 atoms.)
+        else:
+            a_bool = tf.equal(
+                        #--> shape of (len_atoms, len_atoms, num_cutoff)
+                        tf.tile(
+                            tf.expand_dims(Neigh_ind, axis=0),
+                            [len_atoms,1,1],
+                            ),
+                        tf.reshape(tf.range(len_atoms), [len_atoms,1,1]),
+                        )
+
+                    # --> shape of (len_atoms, 3)
+            f_cross = tf.reduce_sum(
+                #--> shape of (len_atoms, len_atoms, 3)
+                tf.reduce_sum(
+                    tf.ragged.boolean_mask(
+                        #--> shape of (len_atoms, len_atoms, num_cutoff, 3)
+                        tf.tile(
+                            tf.expand_dims(
+                                F_ij,
+                                axis=0,
+                                ),
+                            [len_atoms,1,1,1],
+                            ),
+                        #--> shape of (len_atoms, len_atoms, num_cutoff)
+                        a_bool,
+                        name='neigh_mask',
+                        ),
+                    axis=2,
+                    ),
+                axis=1,
+                )
 
         return tf.reshape(f_self + f_cross, [-1, 3])
 
@@ -145,30 +186,30 @@ class ss_calc(Calculator, object):
 
         # Defind some variables.
         type_unique, type_count = np.unique(types, return_counts=True)
+        type_argsort = []
+        for spec in type_unique:
+            type_argsort += list(np.arange(len_atoms)[types == spec])
+        type_argsort_inv = np.zeros(len_atoms, dtype='int32')
+        type_argsort_inv[type_argsort] = np.arange(len_atoms, dtype = 'int32')
+
 
         # Calculate energy and forces.
-        (atomic_energies_not_in_order, forces_not_in_order) = self.sess.run(
+        (atomic_energies_not_in_order, forces) = self.sess.run(
             (self.OL, self.F),
             feed_dict={A:B for A,B in list(zip(self.X,         [fgpt[types == spec] for spec in type_unique])) \
                                     + list(zip(self.X_deriv,   [fgpt_deriv[types == spec] for spec in type_unique])) \
                                     + list(zip(self.Neigh_ind, [neigh_ind[types == spec] for spec in type_unique])) \
+                                    +     [   [self.TAS_inv,   type_argsort_inv]] \
                                     +     [   [self.DR,        1.0]]},
             )
 
         # Concate wrt species.
-        atomic_energies_not_in_order = np.concatenate(atomic_energies_not_in_order, axis=0)
-
-        # Make it in right order.
-        atomic_energies = atomic_energies_not_in_order.copy()
-        forces          = forces_not_in_order.copy()
-
-        start_point = 0
-        for spec in type_unique:
-            atomic_energies[types == spec] = atomic_energies_not_in_order[start_point : start_point + type_count[spec]]
-            forces         [types == spec] = forces_not_in_order         [start_point : start_point + type_count[spec]]
-            start_point += type_count[spec]
+        atomic_energies = np.concatenate(
+            atomic_energies_not_in_order,
+            axis=0,
+            )[type_argsort_inv]
 
         # Save results
-        self.results['atomic_energies'] = atomic_energies
+        self.results['atomic_energies'] = np.squeeze(atomic_energies)
         self.results['energy']          = float(np.sum(atomic_energies))
         self.results['forces']          = forces
